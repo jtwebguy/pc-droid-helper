391ad498afd4326e6509421f2afd624fd086d224
John B <johnboncalos@gmail.com>
2015-03-27 23:13:55 +0800

    Test Message;

---
+++ androidhelper/MainForm.Designer.cs
@@ -0,0 +1,155 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: anakin
+ * Date: 3/27/2015
+ * Time: 1:18 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace androidhelper
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button5;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+			this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(1024, 600);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.picMouseClick);
+			this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.picMouseMove);
+			// 
+			// timer1
+			// 
+			this.timer1.Enabled = true;
+			this.timer1.Interval = 6000;
+			this.timer1.Tick += new System.EventHandler(this.Timer1Tick);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(1042, 107);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(19, 23);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "^";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(1042, 136);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(19, 23);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "v";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(1042, 180);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 3;
+			this.button3.Text = "refresh";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.Button3Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(1042, 225);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(174, 20);
+			this.textBox1.TabIndex = 4;
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(1042, 251);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(75, 23);
+			this.button4.TabIndex = 5;
+			this.button4.Text = "Send";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.Button4Click);
+			// 
+			// button5
+			// 
+			this.button5.Location = new System.Drawing.Point(1042, 280);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(75, 23);
+			this.button5.TabIndex = 6;
+			this.button5.Text = "Enter";
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.Button5Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1231, 626);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.pictureBox1);
+			this.Name = "MainForm";
+			this.Text = "androidhelper";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ androidhelper/MainForm.cs
@@ -0,0 +1,118 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: anakin
+ * Date: 3/27/2015
+ * Time: 1:18 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+using System.IO;
+namespace androidhelper
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void picMouseMove(object sender, MouseEventArgs e)
+		{
+			
+		}
+		void picMouseClick(object sender, MouseEventArgs e)
+		{
+			this.Text = e.X.ToString() + " " + e.Y.ToString();
+			var proc1 = new ProcessStartInfo();
+	        string anyCommand; 
+	        proc1.UseShellExecute = true;
+	
+	        proc1.WorkingDirectory = AppDomain.CurrentDomain.BaseDirectory;
+	
+	        proc1.FileName = @"C:\Windows\System32\cmd.exe";
+	        proc1.Verb = "runas";
+	        proc1.Arguments = "/c adb shell input touchscreen tap " + e.X.ToString() + " " + e.Y.ToString();
+	        proc1.WindowStyle = ProcessWindowStyle.Hidden;
+	        Process.Start(proc1);
+		}
+		void Timer1Tick(object sender, EventArgs e)
+		{
+			
+		}
+		
+		void runcmd(string cmd){
+			var proc1 = new ProcessStartInfo();
+	        proc1.UseShellExecute = true;
+	
+	        proc1.WorkingDirectory = AppDomain.CurrentDomain.BaseDirectory;
+	
+	        proc1.FileName = @"C:\Windows\System32\cmd.exe";
+	        proc1.Verb = "runas";
+	        proc1.Arguments = "/c " + cmd;
+	        proc1.WindowStyle = ProcessWindowStyle.Hidden;
+	        Process.Start(proc1);
+
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			runcmd("adb shell input trackball roll 0 -2");
+		}
+		void Button2Click(object sender, EventArgs e)
+		{
+			runcmd("adb shell input trackball roll 0 2");
+		}
+		void Button3Click(object sender, EventArgs e)
+		{
+			try{
+				this.Text = "trying...";
+				
+					
+					
+				if(File.Exists(AppDomain.CurrentDomain.BaseDirectory + "\\screen.png")){
+					if(pictureBox1.Image != null){
+						pictureBox1.Image.Dispose();
+						pictureBox1.Image = null;
+						
+					}
+					File.Delete(AppDomain.CurrentDomain.BaseDirectory + "\\screen.png");
+				}
+					runcmd("adb shell screencap -p /sdcard/screen.png");
+					Thread.Sleep(500);
+					runcmd("adb pull /sdcard/screen.png");
+					Thread.Sleep(1000);
+					//runcmd("adb shell rm /sdcard/screen.png");
+					//Thread.Sleep(1000);
+					
+					pictureBox1.Image = Image.FromFile(AppDomain.CurrentDomain.BaseDirectory + "\\screen.png");
+					this.Text = "done...";
+				
+			}catch(Exception ex){
+				Debug.Print(ex.Message);
+				this.Text = ex.Message;
+			}
+		}
+		void Button4Click(object sender, EventArgs e)
+		{
+				runcmd("adb shell input text "+textBox1.Text);
+		}
+		void Button5Click(object sender, EventArgs e)
+		{
+			runcmd("adb shell input keyevent ENTER");
+		}
+	}
+}
